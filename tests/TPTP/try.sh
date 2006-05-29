c29b80e0f922339bc73f4ff1f03a1bbea6abf448
Enrico Tassi <enrico.tassi@inria.fr>
2006-05-29 20:41:16 +0000

    fix

--- tests/TPTP/try.sh
+++ tests/TPTP/try.sh
@@ -12,11 +12,11 @@ for X in $TODO; do
   echo -n "$X ... "
   LOGNAME=logs/log.`basename $X`
   ../../matitac.opt $X > $LOGNAME 2>&1
+  RATING=`grep "Rating" $X | sed 's/v.*//' | sed 's/(\*//'`
   if [ `grep "Found a proof" $LOGNAME | wc -l` -gt 0 ]; then
     TIME=`grep "TIME NEEDED" $LOGNAME`
-    RATING=`grep "Rating" $X | sed 's/v.*//' | sed 's/(\*//'`
     echo OK $TIME $RATING
   else
-    echo FAIL
+    echo FAIL $RATING
   fi
 done
