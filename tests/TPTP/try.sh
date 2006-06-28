f12cd978480efb4392af6c0cffe312bf34035bb6
Enrico Tassi <enrico.tassi@inria.fr>
2006-06-28 08:06:34 +0000

    added gzip to log files

--- tests/TPTP/try.sh
+++ tests/TPTP/try.sh
@@ -12,7 +12,7 @@ i=1
 for X in $TODO; do
   echo -n "$X ... "
   LOGNAME=logs/log.`basename $X`
-  ../../matitac.opt $X > $LOGNAME 2>&1
+  ../../matitac.opt -nodb $X | gzip -9 > $LOGNAME 2>&1
   RATING=`grep "Rating" $X | sed 's/v.*//' | sed 's/(\*//'`
   if [ `grep "Found a proof" $LOGNAME | wc -l` -gt 0 ]; then
     TIME=`grep "TIME NEEDED" $LOGNAME`
