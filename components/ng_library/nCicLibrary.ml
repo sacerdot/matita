527c9581d3942cbb8814d4e39cb4abd068ee3560
Ferruccio Guidi <ferruccio.guidi@unibo.it>
2021-05-29 10:43:47 +0200

    nCicLibrary
    + config flag added to emove old objects before storing new ones
      I don't even rememberwhy thisisnot done by default

--- components/ng_library/nCicLibrary.ml
+++ components/ng_library/nCicLibrary.ml
@@ -260,9 +260,10 @@ module Serializer(D: sig type dumpable_s = private #dumpable_status end) =
    let ch = open_out (ng_path_of_baseuri baseuri) in
    Marshal.to_channel ch (magic,(status#dump.dependencies,status#dump.objs)) [];
    close_out ch;
-(*
+
+   if Helm_registry.get_bool "matita.remove_old_objects" then
    remove_objects ~baseuri; (* FG: we remove the old objects before putting the new ones*)
-*)
+
    List.iter
     (function 
      | `Obj (uri,obj) ->
