2dc7311ec21892b41376ad32d29922f9ddc5a3a6
mattia <m@tt.ia>
2023-05-06 09:58:34 +0200

    chore: const def/decl handling

--- components/dedukti/deduktiImport.ml
+++ components/dedukti/deduktiImport.ml
@@ -1,23 +1,13 @@
 let const_tbl = Hashtbl.create 0
 
-let name_and_uri_from_term ~baseuri = function
-    Kernel.Term.Const(_, name) -> let str_ident = Kernel.Basic.string_of_ident (Kernel.Basic.id name) in
-      let uri = NUri.uri_of_string (baseuri ^ "/" ^ str_ident ^ ".con") in
-      (str_ident, uri)
-    | Kernel.Term.App(_, _, _) -> assert false (*TODO what should we return if the term is an App?*)
-    | _ -> assert false (* TODO *) 
-
-let gen_const_name name = name (* TODO do something like "filename/constname" *)
+let mkuri ~baseuri name = 
+  NUri.uri_of_string (baseuri ^ "/" ^ name ^ ".con")
 
 let rec construct_debrujin index = NCic.Rel(index + 1) (* TODO check if it is really 0based*)
 
-and construct_const name =  
-  let cname = gen_const_name name in
-  let reference = (cname, 0) in
-  (* If not already present, register the new constant into the const table *)
-  if !Hashtbl.mem const_tbl cname then
-    Hashtbl.add const_tbl cname reference;
-  NCic.Const reference (*TODO*)
+and construct_const ~baseuri cname =  
+  let reference = Hashtbl.find const_tbl cname in
+  NCic.Const reference
 
 and construct_appl ~baseuri f a1 args =
   let translator = construct_term_and_attrs ~baseuri:baseuri in
@@ -47,17 +37,27 @@ and construct_term_and_attrs ~baseuri = function
   | Kernel.Term.Kind -> assert false
   | Kernel.Term.Type(_) -> assert false
 
-let construct_obj_kind ~baseuri term ident = 
-  let typ = construct_term_and_attrs ~baseuri:baseuri term in
+let construct_obj_kind ~baseuri typ body ident = 
+  let typ = construct_term_and_attrs ~baseuri typ in
+  let body = Option.map (construct_term_and_attrs ~baseuri) body in 
   let attrs = (`Implied, `Axiom, `Regular) in
-  NCic.Constant([], ident, None, typ, attrs)
+  NCic.Constant([], ident, body, typ, attrs)
 
-let constuct_obj ~baseuri term =
-  let (str_ident, uri) = name_and_uri_from_term ~baseuri:baseuri term in
-  (uri, 0, [], [], construct_obj_kind ~baseuri:baseuri term str_ident)
+let constuct_obj status ~baseuri ident typ body =
+  let str_ident = Kernel.Base.string_of_ident ident in 
+  let uri = mkuri ~baseuri str_ident in
+  let obj_kind = construct_obj_kind ~baseuri:baseuri typ body str_ident in
+  let height = NCicTypeChecker.height_of_obj_kind status uri ~subst:[] obj_kind in 
+  let reference = NReference.Ref(uri, if body <> None then Def(height) else Decl) in 
+  (* Register the new constant into the const table *)
+  assert !(Hashtbl.mem const_tbl cname);
+  Hashtbl.add const_tbl cname reference;
+  (uri, 0, [], [], obj_kind)
 
-let rec obj_of_entry ~baseuri = function
-   Parsers.Entry.Def (_,_,_,_,_,term) -> Some(constuct_obj baseuri term) 
+let rec obj_of_entry status ~baseuri = function
+   Parsers.Entry.Def (_,ident,_,_,Some typ,body) -> Some(constuct_obj status baseuri ident typ body) 
+  | Parsers.Entry.Def (_,ident,_,_,None, _) -> assert false
+  | Parsers.Entry.Decl (_,ident,_,_,_,typ) -> Some(constuct_obj status baseuri ident typ None) 
   | _ -> None (*TODO*)
 
 (* TODO: Forse baseuri e' gia' in status *)
@@ -65,7 +65,7 @@ let rec eval_from_dedukti_stream ~asserted ~baseuri status buf =
   try
    let entry = Parsers.Parser.read buf in
    Parsers.Entry.pp_entry Format.err_formatter entry ;
-   let obj = obj_of_entry ~baseuri entry in
+   let obj = obj_of_entry status ~baseuri entry in
    let status = Option.fold ~none:status ~some:(NCicLibrary.add_obj status) obj in
    eval_from_dedukti_stream ~asserted ~baseuri status buf
   with
